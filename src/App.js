dce16097aa5b0d31f5eeaff5f4e7dd7d23d3ada2
lincoln <lincoln-md@hotmail.com>
2021-11-10 23:50:17 -0300

    npx create-react-app + construção do hero

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import hero from './assets/capa.png'
+import Toggle from './components/Toggle'
+import './styles/App.css'
+import './styles/global.css'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <main>
+      <section className="hero">
+        <div>
+          <h1>Lincoln Modesto</h1>
+          <h4>Desenvolvedor Front-end/Mobile</h4>
+        </div>
+        <img src={hero} alt="hero"/>
+      </section>
 
-export default App;
+      <section className="effects">
+        <div className="container-toggle">
+          <h3>Dark Mode</h3>
+          <Toggle/>
+        </div>
+        <div className="container-clock-location">
+          <small>Localização:</small>
+          <h4>Estância/SE 09:30 AM</h4>
+          <div>
+            <div></div>
+            <h6>Trabalhando agora</h6>
+          </div>
+        </div>
+      </section>
+    </main>
+  )
+}
---
+++ src/components/Toggle/index.js
@@ -0,0 +1,10 @@
+import './styles.css'
+
+export default function Toggle() {
+  return (
+    <div class="toggle">
+      <input type="checkbox" id="foo" />
+      <label for="foo"></label>
+    </div>
+  )
+}
