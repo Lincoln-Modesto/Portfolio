7fe898f01fe7023afa961044b4f7add51942b127
lincoln <lincoln-md@hotmail.com>
2021-11-15 21:08:28 -0300

    versão para deploy

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -93,7 +93,7 @@ export function Home({ onToggleTheme }) {
           <Paragraph children={`Olá 👋, sou o Lincoln.
             Sou graduado em Engenharia civil e durante a conclusão do meu curso, desenvolvi o aplicativo móvel (Java e Android nativo) Reescore Lajes registrado junto ao INPI, que é um software voltado a sanar problemas de dimensionamento e projeto das estruturas provisórias de escoramento de lajes maciças e treliçadas.          
             A partir daí, me apaixonei por tecnologia e programação, e venho estudando e fazendo pequenos projetos desde 2020.
-            🚀 Atualmente estou estudando: JavaScript, React JS e React Native, mas também tenho me aventurado em desenvolver aplicações completas, com back-end em node js, e banco de dados com MongoDB e Postgress.`} />
+            🚀 Atualmente estou estudando: JavaScript, React JS e React Native, mas também tenho me aventurado em desenvolver aplicações completas, com back-end em Node JS, e banco de dados com MongoDB e Postgres.`} />
         </div>
       </SectionAbout>
 
